4205b9853188ff1cbd7762f61fdb8b95baad972d
japones <alexandre.homma@gmail.com>
2014-12-24 00:42:53 -0200

    Atualizacoes

--- src/br/com/xkinfo/slc/service/IPessoaService.java
+++ src/br/com/xkinfo/slc/service/IPessoaService.java
@@ -1,23 +1,19 @@
 package br.com.xkinfo.slc.service;
 
 import br.com.xkinfo.slc.modelo.Pessoa;
+import br.com.xkinfo.slc.modelo.Usuario;
 import java.util.ArrayList;
 
 public interface IPessoaService {
 
-    public void incluirPessoa(Pessoa pessoa) throws Exception;
+    public void incluirPessoa(String nome, Integer cpf, String email, Usuario usuario) throws Exception;
 
-    public void alterarPessoa(Pessoa pessoa) throws Exception;
+    public void alterarPessoa(Integer id, String nome, Integer cpf, String email, Usuario usuario) throws Exception;
 
-    public void excluirPessoa(Pessoa pessoa) throws Exception;
+    public void excluirPessoa(Integer id) throws Exception;
 
     public ArrayList<Pessoa> getPessoas() throws Exception;
 
-    public ArrayList<Pessoa> getPessoas(String filtro) throws Exception;
-
     public Pessoa getPessoa(int id) throws Exception;
-
-    public Pessoa getPessoa(String nome) throws Exception;
-
 
 }
--- src/br/com/xkinfo/slc/service/impl/PessoaService.java
+++ src/br/com/xkinfo/slc/service/impl/PessoaService.java
@@ -1,49 +1,57 @@
 package br.com.xkinfo.slc.service.impl;
 
-import br.com.xkinfo.slc.dao.impl.PessoaDAO;
+import br.com.xkinfo.slc.dao.DAOFactory;
 import br.com.xkinfo.slc.modelo.Pessoa;
+import br.com.xkinfo.slc.modelo.Usuario;
 import br.com.xkinfo.slc.service.IPessoaService;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PessoaService implements IPessoaService {
-
-    PessoaDAO pessoaDAO = new PessoaDAO();
+    
+    private Date dataInclusao;
+    private Date dataAlteracao;
 
     @Override
-    public void incluirPessoa(Pessoa pessoa) throws Exception {
-        pessoaDAO.incluirPessoa(pessoa);
+    public void incluirPessoa(String nome, Integer cpf, String email, Usuario usuario) throws Exception {
+        dataInclusao = new Date();
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome(nome);
+        pessoa.setCpf(cpf);
+        pessoa.setEmail(email);
+        pessoa.setUsuarioinclusao(usuario);
+        pessoa.setDatainclusao(dataInclusao);
+        
+        DAOFactory.getPessoaDAO().incluirPessoa(pessoa);
     }
 
     @Override
-    public void alterarPessoa(Pessoa pessoa) throws Exception {
-        pessoaDAO.alterarPessoa(pessoa);
+    public void alterarPessoa(Integer id, String nome, Integer cpf, String email, Usuario usuario) throws Exception {
+        dataAlteracao = new Date();
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(id);
+        pessoa.setNome(nome);
+        pessoa.setCpf(cpf);
+        pessoa.setEmail(email);
+        pessoa.setUsuarioalteracao(usuario);
+        pessoa.setDataalteracao(dataAlteracao);
+        
+        DAOFactory.getPessoaDAO().alterarPessoa(pessoa);
     }
 
     @Override
-    public void excluirPessoa(Pessoa pessoa) throws Exception {
-        pessoaDAO.excluirPessoa(pessoa);
+    public void excluirPessoa(Integer id) throws Exception {
+        DAOFactory.getPessoaDAO().excluirPessoa(getPessoa(id));
     }
 
     @Override
     public ArrayList<Pessoa> getPessoas() throws Exception {
-        return pessoaDAO.getPessoas();
-    }
-
-    @Override
-    public ArrayList<Pessoa> getPessoas(String filtro) throws Exception {
-        return pessoaDAO.getPessoas(filtro);
+        return DAOFactory.getPessoaDAO().getPessoas();
     }
 
     @Override
     public Pessoa getPessoa(int id) throws Exception {
-        return pessoaDAO.getPessoa(id);
+        return DAOFactory.getPessoaDAO().getPessoa(id);
     }
-
-    @Override
-    public Pessoa getPessoa(String nome) throws Exception {
-        return pessoaDAO.getPessoa(nome);
-    }
-
-  
 
 }
