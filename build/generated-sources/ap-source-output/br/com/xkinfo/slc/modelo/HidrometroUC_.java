5b97a49188fa7f8c1e6996eabc8dac94eddf1d68
japones <alexandre.homma@gmail.com>
2014-12-24 00:09:49 -0200

    Ajuste

--- build/generated-sources/ap-source-output/br/com/xkinfo/slc/modelo/HidrometroUC_.java
+++ build/generated-sources/ap-source-output/br/com/xkinfo/slc/modelo/HidrometroUC_.java
@@ -2,22 +2,24 @@ package br.com.xkinfo.slc.modelo;
 
 import java.util.Date;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
-@StaticMetamodel(HidrometroUC.class)
-public abstract class HidrometroUC_ {
+@StaticMetamodel(HidrometroUc.class)
+public abstract class HidrometroUc_ {
 
-	public static volatile SingularAttribute<HidrometroUC, Date> dataultimaleitura;
-	public static volatile SingularAttribute<HidrometroUC, Date> dataretirada;
-	public static volatile SingularAttribute<HidrometroUC, Integer> ultimaleitura;
-	public static volatile SingularAttribute<HidrometroUC, Hidrometro> hidrometro;
-	public static volatile SingularAttribute<HidrometroUC, UnidadeConsumidora> unidadeconsumidora;
-	public static volatile SingularAttribute<HidrometroUC, Integer> id;
-	public static volatile SingularAttribute<HidrometroUC, Date> datainstalacao;
-	public static volatile CollectionAttribute<HidrometroUC, Leitura> leituraCollection;
+	public static volatile SingularAttribute<HidrometroUc, Date> dataultimaleitura;
+	public static volatile SingularAttribute<HidrometroUc, Date> dataretirada;
+	public static volatile SingularAttribute<HidrometroUc, Integer> ultimaleitura;
+	public static volatile SingularAttribute<HidrometroUc, Hidrometro> hidrometro;
+	public static volatile SingularAttribute<HidrometroUc, Usuario> usuarioalteracao;
+	public static volatile SingularAttribute<HidrometroUc, UnidadeConsumidora> unidadeconsumidora;
+	public static volatile SingularAttribute<HidrometroUc, Date> datainclusao;
+	public static volatile SingularAttribute<HidrometroUc, Usuario> usuarioinclusao;
+	public static volatile SingularAttribute<HidrometroUc, Integer> id;
+	public static volatile SingularAttribute<HidrometroUc, Date> dataalteracao;
+	public static volatile SingularAttribute<HidrometroUc, Date> datainstalacao;
 
 }
 
--- build/generated-sources/ap-source-output/br/com/xkinfo/slc/modelo/PessoaUC_.java
+++ build/generated-sources/ap-source-output/br/com/xkinfo/slc/modelo/PessoaUC_.java
@@ -6,15 +6,19 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
-@StaticMetamodel(PessoaUC.class)
-public abstract class PessoaUC_ {
+@StaticMetamodel(PessoaUc.class)
+public abstract class PessoaUc_ {
 
-	public static volatile SingularAttribute<PessoaUC, UnidadeConsumidora> unidadeconsumidora;
-	public static volatile SingularAttribute<PessoaUC, Date> datasaida;
-	public static volatile SingularAttribute<PessoaUC, Pessoa> proprietario;
-	public static volatile SingularAttribute<PessoaUC, Pessoa> inquilino;
-	public static volatile SingularAttribute<PessoaUC, Integer> id;
-	public static volatile SingularAttribute<PessoaUC, Date> dataentrada;
+	public static volatile SingularAttribute<PessoaUc, Usuario> usuarioalteracao;
+	public static volatile SingularAttribute<PessoaUc, UnidadeConsumidora> unidadeconsumidora;
+	public static volatile SingularAttribute<PessoaUc, Date> datasaida;
+	public static volatile SingularAttribute<PessoaUc, Pessoa> proprietario;
+	public static volatile SingularAttribute<PessoaUc, Date> datainclusao;
+	public static volatile SingularAttribute<PessoaUc, Usuario> usuarioinclusao;
+	public static volatile SingularAttribute<PessoaUc, Pessoa> inquilino;
+	public static volatile SingularAttribute<PessoaUc, Integer> id;
+	public static volatile SingularAttribute<PessoaUc, Date> dataalteracao;
+	public static volatile SingularAttribute<PessoaUc, Date> dataentrada;
 
 }
 
