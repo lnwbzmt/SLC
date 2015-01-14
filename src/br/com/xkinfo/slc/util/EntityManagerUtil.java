fa17adcaf83ae636d1af3cf3468c103d87cfb835
japones <alexandre.homma@gmail.com>
2015-01-13 23:38:18 -0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	nbproject/private/private.xml

--- src/br/com/xkinfo/slc/util/EntityManagerUtil.java
+++ src/br/com/xkinfo/slc/util/EntityManagerUtil.java
@@ -1,15 +1,14 @@
 package br.com.xkinfo.slc.util;
 
 import javax.persistence.EntityManager;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EntityManagerUtil {
 
-    private static EntityManagerFactory emf= null;
+    private static EntityManagerFactory emf = null;
     //private static EntityManager em = null;
-    
+
     public static EntityManager getEntityManager() {
         if (emf == null) {
             emf = Persistence.createEntityManagerFactory("SLCPU");
